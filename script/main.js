1b36301d0974a27b4a9df5b0ff0446a054148da1
moonreader <holy_nova@126.com>
2015-07-23 22:03:31 +0800

    美化

--- script/main.js
+++ script/main.js
@@ -1,8 +1,24 @@
+var speed_in_ms = 500;
+
 $(document).ready(function() {
 	init();
 	$('ul>a').each(function(){
 		$(this).on('click',function(){
-			$(this).siblings('li').toggle('fast');
+			// var lis = $(this).siblings('li');
+			if($(this).siblings('li').is(':hidden'))
+			{
+				
+				$('li').hide(speed_in_ms);
+				$(this).siblings('li').show(speed_in_ms);
+
+			}
+			else
+			{
+				$(this).siblings('li').hide(speed_in_ms);
+
+			}
+
+
 		});
 	});
 });
