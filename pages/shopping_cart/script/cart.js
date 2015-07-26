642b937b8cbd4c73b28b526a87f263005f1568e2
moonreader <holy_nova@126.com>
2015-07-26 16:48:41 +0800

    增加css sprite,增加购物车功能

--- pages/shopping_cart/script/cart.js
+++ pages/shopping_cart/script/cart.js
@@ -11,20 +11,23 @@ $(document).ready(function()
 	$('.btn_select_none').on('click',function(){
 		var checkboxs = $('.table_select :checkbox');
 		checkboxs.prop('checked',false);
+		renew_selected_line();
 	});
 	$('.btn_select_all').on('click',function(){
 		var checkboxs = $('.table_select :checkbox');
 		checkboxs.prop('checked',true);
+		renew_selected_line();
 	});
 	$('.btn_select_reverse').on('click',function(){
 		var checkboxs = $('.table_select :checkbox');
 		checkboxs.each(function() {
 			$(this).prop('checked',!$(this).prop('checked'));
-
 		});
+		renew_selected_line();
 	});
 	$('.btn_delete_selected').on('click',delete_selected_lines);
-
+	$('.btn_delete_all').click(delete_all);
+	$('table').on('click',':checkbox',renew_selected_line);
 
 	//------------------------
 	//商品数量增减按钮处理
@@ -33,7 +36,7 @@ $(document).ready(function()
 	//------------------------
 	//删除一行
 	$('table').on('click','.btn_del_line',delete_line);
-	$('#btn_sort_by_num').on('click',sort_by_num);
+
 	
 });
 
@@ -80,7 +83,7 @@ function add_random_good()
 	</tr>\
 	' 
 	$('table').append(a_book);
-	renew_total_line();
+	renew_all();
 	cnt += 1;
 	// renew_all();
 
@@ -94,14 +97,12 @@ function num_add_one()
 	var num = parseInt(my_num_span.text());
 	num += 1;
 	my_num_span.text(num);
-	renew_total_line();
 
 	//数量变化后更新本行的总价
 	var unit_price = parseInt($(this).parent('.table_num').siblings('.table_unit_price').children('span').text());
 	var total_price = unit_price * num;
 	$(this).parent('.table_num').siblings('.table_total').children('span').text(total_price);
-	renew_total_line();
-
+	renew_all();
 }
 //数量减小1
 function num_del_one()
@@ -119,57 +120,67 @@ function num_del_one()
 	var unit_price = parseInt($(this).parent('.table_num').siblings('.table_unit_price').children('span').text());
 	var total_price = unit_price * num;
 	$(this).parent('.table_num').siblings('.table_total').children('span').text(total_price);
-	renew_total_line();
+	renew_all();
 }
 
 
 function renew_all()
 {
-	var all_num = $('.table_all_num span');
-	var all_price = $('.table_all_price span');
-	var num_array = $('.table_good .table_num span').text();
-	for (var i = 0; i < num_array.length; i++)
-	{
-		var temp = num_array[i];
-	};
+	renew_selected_line();
+	renew_total_line();
 
 }
-
-
-function sort_by_price()
+function renew_selected_line()
 {
+	var num = $('.table_good :checked').length;
+	console.log(num+' lines selected');
 
-}
-function sort_by_num()
-{
-	console.log('sort by num');
-	$('.table_num span').each(function(){
-		console.log($(this).index()+":"+$(this).text());
+	var checked_num = 0,checked_price = 0,checked_item = 0;
+	checked_item = $('.table_good :checked').length;
+	$('.table_good :checked').parents('.table_good').each(function(){
+		checked_num += parseInt($(this).find('.table_num span').text());
+		checked_price +=parseInt($(this).find('.table_total span').text());
 	});
+	$('.checked_item span').text(checked_item);
+	$('.checked_num span').text(checked_num);
+	$('.checked_price span').text(checked_price);
 
 }
+
 //刷新合计这一行的数据
 function renew_total_line()
 {
 	var all_num = 0;
 	var all_price = 0;
+	var all_item = 0;
+
 	$('.span_num').each(function(){
 		all_num += parseInt($(this).text());
 	});
-	
 	$('.table_all_num').text('总计'+all_num+'件');
+
+
+
 	$('.span_total_price').each(function(){
 		all_price += parseInt( $(this).text());
 	});
 	$('.table_all_price').text('总计'+all_price+'元');
+
+
+	
+	all_item = $('.table_good input[type="checkbox"]').length;
+	$('.table_all_item').text('总计'+all_item+'条商品');
+
 }
+
+
 
 
 
 function delete_line()
 {
 	$(this).parents('.table_good').remove();
-	renew_total_line();
+	renew_all();
 }
 
 function delete_selected_lines()
@@ -187,10 +198,17 @@ function delete_selected_lines()
 	if(confirm('确定删除'+selected_checkboxs.length+'行商品?'))
 	{
 		selected_checkboxs.parents('.table_good').remove();
+		renew_all();
 		return true;
 	}
-
 }
 
 
-
+function delete_all()
+{
+	if (confirm('确定删除所有商品?'))
+	{
+		renew_all();
+		$('.table_good').remove();
+	};
+}
