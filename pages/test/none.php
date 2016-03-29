5e5bfaba54f320cbe227560ace7790a2c2cbcdd9
moonreader <holy_nova@126.com>
2016-03-29 16:53:06 +0800

    test页面中进行ajax实验

---
+++ pages/test/none.php
@@ -0,0 +1,3 @@
+<?php 
+echo 'php is running'; 
+?>
--- pages/test/test.js
+++ pages/test/test.js
@@ -1,64 +1,58 @@
-window.addEventListener('load',window_load_handler,false);
-function window_load_handler(){
+window.addEventListener('load', window_load_handler, false);
+
+function window_load_handler()
+{
 	console.log('test.js is ready');
-	str_cat('one','two','three');
-	// str_cat('one','two',3);
-	// str_cat('one','two',0);
-	str_cat('one','two','0');
+
+	my_json = [
+	{
+		"title": "Journey to the west",
+		"authors": ["Wu Chengen"],
+		year:1997
+	},
+	{
+		"title": "Red Building",
+		"authors": ["Cao","Gao"],
+		year:1998
+	},
+	{
+		"title": "Three Kindoms",
+		"authors": ["Luo,Guanzhong"],
+		"year":1997
+	}];
+	// books = JSON.parse(my_json);
+	books = my_json;
+	for(var i=0,max=books.length;i<max;i++)
+	{
+		console.log('title:'+books[i].title);
+		console.log('authors:'+books[i].authors);
+		console.log('year:'+books[i].year);
+	}
+
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4){
+			if((xhr.status >= 200 && xhr.status <300) || xhr.status == 304)
+			{
+				console.log('success '+xhr.responseText);
+				console.log('xhr.status = '+xhr.status);
+				document.getElementById('from_server').innerHTML = xhr.responseText;
+			}
+			else
+			{
+				console.log('request is unsuccessful xhr.status = ' +xhr.status);
+			}
+		}
+	};
 
 	try
 	{
-		str_cat('one ','two ',3);
+		xhr.open('get', 'http://localhost/test.php?name=sang&addr=china', true);
+		xhr.send(null);
 	}
-	catch(error)
+	catch (error)
 	{
-		console.log('Error caught:'+error.message);
+		console.log(error.message);
 	}
-	finally{
-		console.log('finally come here');
 
-	}
-	var arr =['sang','yi',999,'100','min'];
-	str_cat_super('1','2','3','4',5,6,'7',arr,'9');
-
-}
-
-function str_cat(str1,str2,str3){
-	var str = str1 + str2;
-	if (typeof str3 === 'string')
-	{
-		str += str3;
-	}
-	else
-	{
-		throw new Error('str_cat(): str3 is not a string');
-	}
-	console.log(str);
-	return str;
-}
-
-function str_cat_super(){
-	var length = arguments.length;
-	if(length>0)
-	{	
-		var result='';
-		for(var i=0;i<length;i++)
-		{
-			if(typeof arguments[i] === "string")
-			{
-				result = result +arguments[i]+',';
-			}
-			else if(typeof arguments[i] === 'array')
-			{
-				result += arguments[i].join(',');	
-			}
-			else if(arguments[i] instanceof Array)
-			{
-				result += arguments[i].join(',');
-				console.log('instanceof Array');
-			}
-		}
-		console.log(result);
-		return result;
-	}
 }
