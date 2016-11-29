1d24f9dff6d3c5c97e4cb76956dd48949d689270
moonreader <holy_nova@126.com>
2016-11-29 14:20:39 +0800

    完成react井字棋

--- framework/react_tiktak/game.js
+++ framework/react_tiktak/game.js
@@ -1,74 +1,135 @@
 class GameCell extends React.Component {
-	constructor(props) {
-		super(props);
-
-	}
-	render() {
-		const value = this.props.value;
-		return (<div className = 'cell' onClick = {this.props.onClick}>{value}</div>);
-	}
+    render() {
+        const value = this.props.value;
+        return (<div className = {this.props.isWinner? 'cell winner':'cell'} onClick = {this.props.onClick}>{value}</div>);
+    }
 }
 class InfoBox extends React.Component {
-	render() {
-
-		return (<p> next move:{ this.props.isNextX?"X":"O"}</p>);
-	}
+    render() {
+        // return (<p> next move:{ this.props.isNextX?"X":"O"}</p>);
+        return (<div className = 'infobox'> {this.props.info}</div>);
+    }
 }
 class GameBox extends React.Component {
-	findWinner() {
-		//find winner
-		//
-		var cells = this.state.cellData;
-		var mod = 3;
-		0 1 2
-		3 4 5
-		6 7 8
-		var len = cells.length;
-
-		for (var row = 0; row < len / mod; row++) {
-			for (var j = 0; j < len / mod; j++) {
-
-			}
-		}
-	}
-	constructor(props) {
-		super(props);
-		this.state = {
-			cellData: new Array(9).fill(null),
-			isNextX: true
-		};
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-
-	render() {
-		var cellData = this.state.cellData;
-		var cells = cellData.map((value, index) => (<GameCell 
+    findWinner(cells) {
+        //find winner
+        //result :
+        //1. X----x win
+        //2. O ----o win
+        //3. draw --- full and draw
+        //4. running --- 
+        // var cells = this.state.cellValues;
+        // var mod = 3;
+        // 0 1 2
+        // 3 4 5
+        // 6 7 8
+        var winTypes = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+        for (var i = 0; i < winTypes.length; i++) {
+            let type = winTypes[i];
+            if (cells[type[0]] !== null && cells[type[0]] === cells[type[1]] && cells[type[0]] === cells[type[2]]) {
+                var arrIsWinner = this.state.arrIsWinner.slice();
+                for (var j = 0; j < type.length; j++) {
+                    arrIsWinner[type[j]] = true;
+                }
+                this.setState({
+                    arrIsWinner: arrIsWinner
+                });
+                return cells[type[0]];
+            }
+        }
+        // 所有都不为空 
+        if (cells.every((cell) => (cell !== null))) {
+            return 'draw';
+        } else {
+            return 'running';
+        }
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            cellValues: new Array(9).fill(null),
+            arrIsWinner: new Array(9).fill(false),
+            isNextX: true,
+            info: '下一步:X',
+            isGameover: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.restart = this.restart.bind(this);
+    }
+    render() {
+        var cellValues = this.state.cellValues;
+        var cells = cellValues.map((value, index) => (<GameCell 
 			key = {index} 
 			value={value}
+			isWinner = {this.state.arrIsWinner[index]}
 			onClick = {()=>{this.handleClick(index)}} />));
-		return (
-			<div>
-			<InfoBox isNextX = {this.state.isNextX}/>
+        return (
+            <div>
+                <h2 className='desc'>React.js井字棋</h2>
+			<InfoBox info = {this.state.info} />
 			<div className = 'box'> 
 			{cells} 
 			</div>
+			<input type = 'button' value = '重新开始' onClick = {this.restart}/>
 			</div>);
-	}
-	handleClick(index) {
-		//make a copy of cellData
-		var data = this.state.cellData.slice();
-		data[index] = this.state.isNextX ? "X" : "O";
-		var isNextX = this.state.isNextX;
-		this.setState({
-			cellData: data,
-			isNextX: !isNextX
-		})
-	}
-
+    }
+    restart() {
+        this.setState({
+            cellValues: new Array(9).fill(null),
+            arrIsWinner: new Array(9).fill(false),
+            isNextX: true,
+            info: '下一步:X',
+            isGameover: false
+        });
+    }
+    handleClick(index) {
+        //make a copy of cellValues
+        if (this.state.isGameover || this.state.cellValues[index]) {
+            return;
+        }
+        var data = this.state.cellValues.slice();
+        data[index] = this.state.isNextX ? "X" : "O";
+        var isNextX = !this.state.isNextX;
+        this.setState({
+            cellValues: data,
+            isNextX: isNextX,
+        })
+        var winner = this.findWinner(data);
+        var info = this.state.info;
+        switch (winner) {
+            case 'X':
+            case 'O':
+                info = '胜利者:' + winner;
+                // 结束游戏
+                this.setState({
+                    isGameover: true
+                });
+                break;
+            case 'running':
+                info = '下一步:' + (isNextX ? 'X' : 'O');
+                break;
+            case 'draw':
+                info = '平局';
+                this.setState({
+                    isGameover: true
+                });
+                break;
+        }
+        this.setState({
+            info: info
+        });
+    }
 }
-
 ReactDOM.render(
-	<GameBox />,
-	document.getElementById('root')
+    <GameBox />,
+    document.getElementById('root')
 );
